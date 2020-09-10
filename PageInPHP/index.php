dd1dbad253a906f435ed73843b6289635ba7f5f7
Douglas Mendes <capudougmor@gmail.com>
2020-09-10 18:38:17 -0300

    Create project PageInPHP

---
+++ PageInPHP/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="palavras chaves">
+  <meta name="description" content="Descrição do site">
+
+  <link href="styles/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  
+  <title>Projeto 01</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">logomarca</div>
+    <div class="center">
+      <nav class="navbar">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Sobre</a></li>
+          <li><a href="">Serviços</a></li>
+          <li><a href="">Contato</a></li>
+        </ul>
+      </nav>
+  </header>
+
+  <section>
+    <div class="center">
+      <form action="" class="bannerPrincipal">
+        <h2>Qual seu melhor e-mail</h2>
+        <input type="email" name="email" require>
+        <input type="submit" name="acao" value="Cadastrar!">
+      </form>
+    </div>
+  </section>
+
+  <section class="authorDescription">
+    <div class="center">
+      <div class="center">
+        <div class="w50">
+          <h2>Douglas Mendes Assunção</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto quisquam veritatis quia distinctio pariatur, earum est, provident blanditiis quo laborum neque maxime accusamus saepe, sint doloribus in quas! Mollitia, odit!</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto quisquam veritatis quia distinctio pariatur, earum est, provident blanditiis quo laborum neque maxime accusamus saepe, sint doloribus in quas! Mollitia, odit!</p>
+        </div>
+
+        <div class="w50">
+          <img src="images/IMG-20190504-WA0027.jpg" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <section class="specialties">
+    <div class="center">
+      <h2 class="title">Especialidades</h2>
+      <div class="specialties">
+        <i class="fa fa-html5" aria-hidden="true"></i>
+        <h3>HTML</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sed repellendus voluptatem odit dolorem autem quis quidem nostrum esse nihil est assumenda ducimus cupiditate, rem ut excepturi officia optio doloremque!</p>
+      </div>
+
+      <div class="specialties">
+        <i class="fa fa-css3" aria-hidden="true"></i>
+        <h3>CSS3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sed repellendus voluptatem odit dolorem autem quis quidem nostrum esse nihil est assumenda ducimus cupiditate, rem ut excepturi officia optio doloremque!</p>
+      </div>
+
+      <div class="specialties">
+        <i class="fa fa-cube" aria-hidden="true"></i>
+        <h3>PHP</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel sed repellendus voluptatem odit dolorem autem quis quidem nostrum esse nihil est assumenda ducimus cupiditate, rem ut excepturi officia optio doloremque!</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="extras">
+    <div class="center">
+      <div class="w50">        
+        <h2 class="title">Depoimentos</h2>
+        <div class="singleDepositions">
+          <p class="depositionText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione sunt quas cupiditate quod debitis, laboriosam ad magnam, fugit hic dignissimos neque odit quasi sequi, est illum qui nesciunt omnis alias?</p>
+          <p class="autor">Mamae</p>
+        </div>
+        
+        <div class="singleDepositions">
+          <p class="depositionText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione sunt quas cupiditate quod debitis, laboriosam ad magnam, fugit hic dignissimos neque odit quasi sequi, est illum qui nesciunt omnis alias?</p>
+          <p class="autor">Mamae</p>
+        </div>
+        
+        <div class="singleDepositions">
+          <p class="depositionText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione sunt quas cupiditate quod debitis, laboriosam ad magnam, fugit hic dignissimos neque odit quasi sequi, est illum qui nesciunt omnis alias?</p>
+          <p class="autor">Mamae</p>
+        </div>
+      </div>
+
+      <div class="w50">        
+        <h2 class="title">Serviços</h2>
+        <div class="services">
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, cumque.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, cumque.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, cumque.</li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="center">
+      <p>todos os direitos reservados</p>
+    </div>
+  </footer>
+</body>
+
+</html>
